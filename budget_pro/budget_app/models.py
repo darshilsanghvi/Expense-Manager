09234bcd351ac2a8af7ee218cb7b2ec2ee6e6083
darshilsanghvi <darshilsanghvi1818@gmail.com>
2021-01-01 11:50:38 +0530

    commit mesage

--- budget_pro/budget_app/models.py
+++ budget_pro/budget_app/models.py
@@ -5,6 +5,9 @@ class expense(models.Model):
     expense_type = models.CharField(max_length=40)
     expense_amnt = models.IntegerField()
     expense_day = models.CharField(max_length=15)
-    expense_date = models.DateField(auto_now=False)
-
+    expense_date = models.CharField(max_length=20)
+    
+class budget(models.Model):
+    total_budget = models.IntegerField()
+    left_budget = models.IntegerField()
 
